d087433061318a7cf6c4fc680bc04c14329cf17b
Rodrigo Henrique <rodrigo.henrique@iatec.com>
2025-10-19 19:36:07 -0300

    refactor: restructure user registration test and add contact form test with dynamic data

--- cypress/e2e/3-automation-exercise/automation-exercise.cy.js
+++ cypress/e2e/3-automation-exercise/automation-exercise.cy.js
@@ -3,12 +3,13 @@ const { faker } = require('@faker-js/faker');
 const { expect } = require('chai');
 
 describe('Automation exercise', () => {
+    beforeEach(() => {
+        cy.viewport('samsung-s10');
+        cy.visit('https://automationexercise.com/');
+    });
+
     it('Cadastrar usuário', () => {
         const timestamp = new Date().getTime();
-        // Altera a responsividade
-        cy.viewport('samsung-s10');
-
-        cy.visit('https://automationexercise.com/');
         cy.get('a[href="/login"]').click();
         cy.get('input[data-qa="signup-email"]').type(`sheldon-${timestamp}@mailinator.com`);
         cy.get('input[data-qa="signup-name"]').type("Sheldon Lee Cooper");
@@ -42,17 +43,42 @@ describe('Automation exercise', () => {
         cy.get('input[data-qa="zipcode"]').type(faker.location.zipCode());
         cy.get('input[data-qa="mobile_number"]').type(faker.phone.number());
 
-        cy.contains('button','Create Account').click();
+        cy.contains('button', 'Create Account').click();
 
         // Assert
-        cy.url().should('includes','account_created');
-        cy.contains('b','Account Created!');
+        cy.url().should('includes', 'account_created');
+        cy.contains('b', 'Account Created!');
 
 
 
     });
     it('Login', () => {
         cy.loginUsuario();
+    });
+
+    it.only('Contact us', () => {
+        cy.log(`FILL THE FORM ${faker.person.fullName()}`);
+        cy.get('a[href="/contact_us"]').click();
+        cy.get('input[data-qa="name"]').type(faker.person.fullName());
+        cy.get('input[data-qa="email"]').type(faker.internet.email());
+        cy.get('input[data-qa="subject"]').type("Teste Automatizado de envio de formulário com anexo");
+        cy.get('textarea[data-qa="message"]').type(faker.lorem.paragraphs(1));
+        
+        cy.log('UPLOAD THE IMAGE');
+        cy.fixture('iatec.jpeg').as('imagem-iatec');
+        cy.get('input[name="upload_file"]').selectFile('@imagem-iatec');
+
+        cy.log('SUBMIT FORM');
+        cy.get('input[data-qa="submit-button"]').click();
+
+        cy.log('ASSERTS');
+        cy.contains('div','Success! Your details have been submitted successfully.'); //Indireta
+        cy.contains('span',' Home'); // Indireta
+        cy.get('.status').should('be.visible'); //Somente com should que aparece o destaque
+        cy.get('.status').should('have.text','Success! Your details have been submitted successfully.');
+        cy.get('div[class="status alert alert-success"]').should('have.text','Success! Your details have been submitted successfully.');
+        cy.get('a[class="btn btn-success"]').should('be.visible');
+
     });
 
 

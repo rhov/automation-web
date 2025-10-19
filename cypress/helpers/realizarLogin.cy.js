30c7a8102511790a281e772a69ce3bb6ada9af9c
Rodrigo Henrique <rodrigo.henrique@iatec.com>
2025-10-19 18:34:49 -0300

    refactor: remove realizarLogin test as it is no longer needed

--- cypress/helpers/realizarLogin.cy.js
+++
@@ -1,9 +0,0 @@
-describe('', () => {
-    it('Cadastrar usuário', () => {
-        cy.visit('https://automationexercise.com/');
-        cy.get('a[href="/login"]').click();
-        cy.get('input[data-qa="login-email"]').type("sheldon@mailinator.com");
-        cy.get('input[data-qa="login-password"]').type("sheldon@123");
-        cy.get('button[data-qa="login-button"]').click();
-    });
-});
